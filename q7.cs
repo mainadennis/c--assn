f80a652f27ee06b475ed3addcedd31e8e178df8b
Charles <53397655+charlesncn@users.noreply.github.com>
2022-02-01 07:35:37 +0300

    Create q7.cs

---
+++ q7.cs
@@ -0,0 +1,31 @@
+using System;
+public class question7
+{
+  public static void Main()
+  {
+    float distance;
+    float hour, min, sec;
+     
+    float timeSec;
+    float mps;
+    float kph, mph;
+     
+    Console.Write("Input distance(metres): ");
+    distance = Convert.ToSingle(Console.ReadLine());
+    Console.Write("Input timeSec(hour): ");
+    hour = Convert.ToSingle(Console.ReadLine());
+    Console.Write("Input timeSec(minutes): ");
+    min = Convert.ToSingle(Console.ReadLine());
+    Console.Write("Input timeSec(seconds): ");
+    sec = Convert.ToSingle(Console.ReadLine());
+     
+    timeSec = (hour*3600) + (min*60) + sec;
+    mps = distance/timeSec;
+    kph = (distance/1000.0f)/(timeSec/3600.0f);
+    mph = kph / 1.609f;
+     
+    Console.WriteLine("Your speed in metres/sec is {0}", mps);
+    Console.WriteLine("Your speed in km/h is {0}", kph);
+    Console.WriteLine("Your speed in miles/h is {0}", mph);
+  }
+}
