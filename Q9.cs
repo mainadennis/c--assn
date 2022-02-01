cab35f5bbd15790ea2b51cb336ad641db751e5f6
Charles <53397655+charlesncn@users.noreply.github.com>
2022-02-01 07:38:46 +0300

    Create Q9.cs

---
+++ Q9.cs
@@ -0,0 +1,12 @@
+using System;
+
+class MainClass {
+    public static void Main (string[] args) {
+    Console.WriteLine ("Please enter weight in grams:");
+    double g = Convert.ToDouble(Console.ReadLine());
+    double kg = g /1000;
+    double howMany = kg/1000 
+    Console.WriteLine(kg+ " Kgs");
+    Console.WriteLine(howMany+ " packets make 1kg");
+  }
+}
